eaac376e5c90da31125eacafb065bdd543cc7621
Ekramul Nibir <ekramulnibir@Ekramuls-Laptop.local>
2024-08-19 16:13:33 +0600

    Django-Forms Module 13 Added

--- Project_3/first_app/views.py
+++ Project_3/first_app/views.py
@@ -5,19 +5,20 @@ from datetime import datetime
 def home(request):
     d = {'author' : 'Rahim', 'age': 20, 'list': ['Python', 'is', 'fun'], 'courses': [
         {
-            'id': 1,
             'name': 'Python',
+            'id': 1,
             'fee' : 5000
         },
         {
-            'id': 2,
             'name': 'Django',
+            'id': 2,
             'fee' : 6500
         },
         {
-            'id': 3,
             'name': 'Data Science',
+            'id': 3,
             'fee' :10000
         },
-    ], 'date': datetime.now(), 'empty_string:': ''}
+    ], 'date': datetime.now(), 'empty_string:': '', 'name':'nibir ekramul', 'name_char':['n', 'i', 'b', 'i', 'r', ' ', 'e', 'k', 'r', 'a', 'm', 'u', 'l'], 'animals':['cat', 'dog', 'horse']
+    }
     return render(request, 'home.html', context = d)
