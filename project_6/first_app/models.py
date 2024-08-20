a89a0aae992f11a29cabf5ffb07b29d4d4c81957
Ekramul Nibir <ekramulnibir@Ekramuls-Laptop.local>
2024-08-20 16:21:05 +0600

    Module 14-Database

---
+++ project_6/first_app/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+class Student(models.Model):
+    name = models.CharField(max_length= 20)
+    roll = models.IntegerField(primary_key= True)
+    address = models.TextField()
+    father_name = models.TextField(default="Nibir")
+
+    def __str__(self) -> str:
+        return f"Roll: {self.roll} - {self.name }"
